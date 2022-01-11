c1ce2c6a9e482c050a9307b26ab328cd41b1ba97
dmitrytsh <mdsmf@gmail.com>
2022-01-11 18:15:41 +0200

    install router, add router.js

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import "/views/HomeView";
+import "/views/AboutView";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>It's a home component</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+
+</style>
