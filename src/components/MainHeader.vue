a13fa8986ffaa8c1eaff9be986ce9878ab157322
dmitrytsh <mdsmf@gmail.com>
2022-01-13 15:05:51 +0200

    problem with flexbox, help plz

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,8 +1,14 @@
 <template>
   <ul id="menu">
-    <li id="home"><router-link to="/"><img src="../assets/img/pinkPony.jpeg" height="23" width="25" alt="Home"/></router-link></li>
-    <li><router-link to="/faq">Faq</router-link></li>
-    <li><router-link to="/about">About</router-link></li>
+    <li id="home">
+      <router-link to="/"><img src="../assets/img/pinkPony.jpeg" height="25" width="25" alt="Home"/></router-link>
+    </li>
+    <li class="navi">
+      <router-link to="/faq">Faq</router-link>
+    </li>
+    <li class="navi">
+      <router-link to="/about">About</router-link>
+    </li>
   </ul>
 </template>
 
@@ -14,51 +20,50 @@ export default {
 </script>
 
 <style scoped>
-li{
-  display: inline-block;
+ .navi {
+  display: inline-flex;
+  justify-content: center;
+   justify-self: center;
+   justify-items: center;
+  /*flex-direction: row;*/
+   margin: 0;
+   padding: 0;
 }
-#menu, #menu li {
-
-  margin: 0; /* убрать внутренние отступы */
-
-  padding: 0; /* убрать внешние отступы */
-
-}
+ #home {
+   display: inline-flex;
+   justify-content: left;
+   justify-self: left;
+   justify-items: left;
+   /*flex-direction: row;*/
+   margin: 0;
+   padding: 0;
+ }
 
 #menu {
-  padding: 0;
-  text-align: center;
+  margin: 0; /* убрать внутренние отступы */
+  padding: 0; /* убрать внешние отступы */
+  /*padding: 0;*/
+  /*text-align: center;*/
   background: rgba(204, 138, 181, 0.85); /* цвет фона навигационной панели */
 
 }
-#home{
-  float: left;
-  margin: 0;
-}
 
-#menu li{
 
-  display:inline-block; /* расположить пункты горизонтально */
-
-  text-align: left; /* текст горизонтально по центру */
-
-}
+/*#menu li {*/
+/*  display: inline-block; !* расположить пункты горизонтально *!*/
+/*  flex-direction: row;*/
+/*  justify-content: center;*/
+/*  text-align: center; !* текст горизонтально по центру *!*/
+/*}*/
 
 #menu a {
-
-  display: block; /* ссылка растягивается на весь пункт li */
-
+  display: inline-block; /* ссылка растягивается на весь пункт li */
   padding: 5px 15px; /* задаем внутренние отступы */
-
   color: #fff; /* цвет текста */
-
   text-decoration: none; /* убрать нижнее подчёркивание у ссылок */
-
 }
 
 #menu a:hover {
-
   background: #8AB8CC; /* фон пунктов при наведении */
-
 }
 </style>
