4f68c948b853c3fb9d434645eca3a81dbe34f5ad
Dmytro_Shlyhin <dmitro_shlyhin@epam.com>
2022-01-17 19:52:27 +0200

    update menu style

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <ul id="menu">
-    <li id="home">
-      <router-link to="/"><img src="../assets/img/pinkPony.jpeg" height="25" width="25" alt="Home"/></router-link>
-    </li>
-    <li class="navi">
-      <router-link to="/faq">Faq</router-link>
-    </li>
-    <li class="navi">
-      <router-link to="/about">About</router-link>
-    </li>
-  </ul>
+  <div class="menu">
+    <router-link class="header-img" to="/"><img src="../assets/img/pinkPony.jpeg" height="25" width="25" alt="Home"/></router-link>
+    <ul class="header-link">
+      <li class="nav">
+        <router-link to="/faq">Faq</router-link>
+      </li>
+      <li class="nav">
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 
@@ -20,50 +20,23 @@ export default {
 </script>
 
 <style scoped>
- .navi {
-  display: inline-flex;
-  justify-content: center;
-   justify-self: center;
-   justify-items: center;
-  /*flex-direction: row;*/
+ .menu{
+   padding: 2px;
    margin: 0;
-   padding: 0;
-}
- #home {
-   display: inline-flex;
-   justify-content: left;
-   justify-self: left;
-   justify-items: left;
-   /*flex-direction: row;*/
-   margin: 0;
-   padding: 0;
+   display: flex;
+   background-color: rgba(204, 138, 181, 0.85);
+   justify-content: center;
  }
-
-#menu {
-  margin: 0; /* убрать внутренние отступы */
-  padding: 0; /* убрать внешние отступы */
-  /*padding: 0;*/
-  /*text-align: center;*/
-  background: rgba(204, 138, 181, 0.85); /* цвет фона навигационной панели */
-
-}
-
-
-/*#menu li {*/
-/*  display: inline-block; !* расположить пункты горизонтально *!*/
-/*  flex-direction: row;*/
-/*  justify-content: center;*/
-/*  text-align: center; !* текст горизонтально по центру *!*/
-/*}*/
-
-#menu a {
-  display: inline-block; /* ссылка растягивается на весь пункт li */
-  padding: 5px 15px; /* задаем внутренние отступы */
-  color: #fff; /* цвет текста */
-  text-decoration: none; /* убрать нижнее подчёркивание у ссылок */
-}
-
-#menu a:hover {
-  background: #8AB8CC; /* фон пунктов при наведении */
-}
+ .header-link{
+   display: inline-flex;
+   flex-direction: row;
+ }
+ .header-img{
+   position: absolute;
+   top: 1%;
+   left: 11px;
+ }
+ .nav{
+   margin-right: 20px;
+ }
 </style>
